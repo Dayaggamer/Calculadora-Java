b409cc974776b07c2756123da20c88840ff1f46b
Dayaggamer <90013535+Dayaggamer@users.noreply.github.com>
2022-01-04 18:47:59 -0500

    Calculadora Basica

---
+++ CalculadoraJava/src/calculadorajava/CalculadoraJava.java
@@ -0,0 +1,17 @@
+package calculadorajava;
+
+/**
+ *
+ * @author DAYA
+ */
+public class CalculadoraJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmCalculadora c =new frmCalculadora();
+        c.setVisible(true);
+    }
+    
+}
